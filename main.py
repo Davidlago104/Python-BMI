2de1d0eaef64bb462ca3e6ffc63520b6414bd9cd
David Lago <david@macbook-pro.myfiosgateway.com>
2026-04-24 22:27:30 -0400

    added if else to bmi

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-kg = int(input("Enter the weight in kilograms: "))
+kg = float(input("Enter the weight in kilograms: "))
 
 height = float(input("Enter the height in meters: "))
 
 bmi = kg / (height ** 2)
 
+if bmi < 18.5:
+    print("You are underweight.")
+elif 18.5 <= bmi < 25:
+    print("You are normal weight.")
+else: print("You are Overweight.")
 print("Your BMI is: ", bmi)
